a807813b8b03dc79d1bae575823fac22f7fe3beb
Elena <ellevanova@gmail.com>
2021-05-29 00:50:05 +0400

    reworked program and added tests

--- Fundamentals.Test/ProgramTests.cs
+++ Fundamentals.Test/ProgramTests.cs
@@ -11,5 +11,37 @@ namespace Fundamentals.Test
         {
             Program calc = new Program();
         }
+        [Fact]
+        public void ShouldInitConsoleWrapper()
+        {
+            Program calc = new Program();
+            ConsoleWrapper console = new ConsoleWrapper("n");
+            Assert.Equal(double.NaN, console.NumInput1);
+            Assert.Equal(double.NaN, console.NumInput2);
+            Assert.Equal("", console.Operation);
+            Assert.Equal("n", console.SimbolOut);
+        }
+        [Fact]
+        public void ShouldReadConsole()
+        {
+            ConsoleWrapper cns = new ConsoleWrapper("n");
+            string input = ConsoleWrapper.GetConsoleRead();
+
+        }
+
+        [Fact]
+        public void ShouldGetInput1()
+        {
+            ConsoleWrapper cns = new ConsoleWrapper("n");
+            double input = cns.GetNumInput();
+
+        }
+
+        [Fact]
+        public void ShouldCalculateResult()
+        {
+            ConsoleWrapper cns = new ConsoleWrapper("n");
+            double input = cns.CalculateResult();            
+        }
     }
 }
--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -6,6 +6,68 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
+            ConsoleWrapper consoleWrapper = new ConsoleWrapper("n");
+            consoleWrapper.StartCalculator();
+        }
+    }
+
+
+        public class ConsoleWrapper
+    {
+        public double NumInput1 { set; get; }
+        public double NumInput2 { set; get; }
+        public string Operation { set; get; }
+        public string SimbolOut { set; get; }
+
+        public ConsoleWrapper(string simbolOut)
+        {
+            NumInput1 = double.NaN;
+            NumInput2 = double.NaN;
+            Operation = "";
+            SimbolOut = simbolOut;
+        }
+        public static string GetConsoleRead()
+        {
+            string input = Console.ReadLine();
+            if (input != "") return input;
+            else return "";
+        }
+        public double GetNumInput() 
+        { 
+            // Ask the user to type the number.
+            Console.Write("Type a number, and then press Enter: ");
+            string numInput1 = ConsoleWrapper.GetConsoleRead();
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+                numInput1 = ConsoleWrapper.GetConsoleRead();
+                if (numInput1 == SimbolOut) return double.NaN;
+                else break;
+            }
+            return cleanNum1;
+        }
+        public double CalculateResult()
+        {
+            NumInput1 = this.GetNumInput();
+            NumInput2 = this.GetNumInput();
+            double result = 0;
+
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? ");
+
+            Operation = ConsoleWrapper.GetConsoleRead();
+            result = Calculator.DoOperation(NumInput1, NumInput2, Operation);
+            return result;
+
+        }
+        public void StartCalculator()
+        {
             bool endApp = false;
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
@@ -13,46 +75,12 @@ namespace Fundamentals
 
             while (!endApp)
             {
-                // Declare variables and set to empty.
-                string numInput1 = "";
-                string numInput2 = "";
-                double result = 0;
-
-                // Ask the user to type the first number.
-                Console.Write("Type a number, and then press Enter: ");
-                numInput1 = Console.ReadLine();
-
-                double cleanNum1 = 0;
-                while (!double.TryParse(numInput1, out cleanNum1))
-                {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
-                    numInput1 = Console.ReadLine();
-                }
-
-                // Ask the user to type the second number.
-                Console.Write("Type another number, and then press Enter: ");
-                numInput2 = Console.ReadLine();
-
-                double cleanNum2 = 0;
-                while (!double.TryParse(numInput2, out cleanNum2))
-                {
-                    Console.Write("This is not valid input. Please enter an integer value: ");
-                    numInput2 = Console.ReadLine();
-                }
-
-                // Ask the user to choose an operator.
-                Console.WriteLine("Choose an operator from the following list:");
-                Console.WriteLine("\ta - Add");
-                Console.WriteLine("\ts - Subtract");
-                Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
-                Console.Write("Your option? ");
-
-                string op = Console.ReadLine();
+                
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    double result=this.CalculateResult();
+                    
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -67,12 +95,15 @@ namespace Fundamentals
                 Console.WriteLine("------------------------\n");
 
                 // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
+                Console.Write("Press "+SimbolOut+" and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == SimbolOut) endApp = true;
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
             return;
-        }
+        
+
+    }
+
     }
 }
